ce727be6b07a262acab030e4ecb2ed3b832b8fd2
Atte Perkio <perkioatte@gmail.com>
2026-01-17 12:15:09 +0200

    Delete swap member function

--- include/quicksort.h
+++ include/quicksort.h
@@ -11,7 +11,6 @@ public:
     void sort(std::vector<int>& arr);
 private: 
     void quicksort(std::vector<int>& arr, int left, int right);
-    void swap(int& x, int& y);
     int partition(std::vector<int>& arr, int left, int right);
     std::vector<Event>& m_events;
 };
