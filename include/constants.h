f1768dd02164f654021add724f7680a146f5bb49
Atte Perkio <perkioatte@gmail.com>
2026-01-17 13:03:48 +0200

    Add file for constants

---
+++ include/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+    inline constexpr int windowWidth =  800; // SFML window width
+    inline constexpr int windowHeight = 600; // SFML window height
+
+    inline constexpr float barWidth = 40.f;
+    inline constexpr int barHeightMultiplier = 50;
+    inline constexpr float barOutline = 2.f;
+
+}
+
+#endif
